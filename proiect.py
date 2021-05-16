cc422e81cc6101d80e54ff03ae33590fadaf52dd
RotaruGeorge <83924463+RotaruGeorge@users.noreply.github.com>
2021-05-17 01:17:41 +0300

    Add files via upload

--- proiect.py
+++ proiect.py
@@ -1,4 +1,35 @@
 import cv2
+import numpy as np
+from tkinter import *
+from tkinter import filedialog
+import os
+import tkinter as tk
+from PIL import Image, ImageTk
+def showimage():
+    fln = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select image file", filetypes=(("JPG File", "*.jpg"), ("PNG file", "*.png"), ("All File", "*.*")))
+    img = Image.open(fln)
+    img.thumbnail((350,350))
+    img = ImageTk.PhotoImage(img)
+    lbl.configure(image=img)
+    lbl.image = img
+
+root = Tk()
+
+frm = Frame(root)
+frm.pack(side=BOTTOM, padx=15, pady=15)
+
+lbl= Label(root)
+lbl.pack()
+
+btn = Button(frm, text="browser image",command=showimage)
+btn.pack(side=tk.LEFT)
+
+btn = Button(frm, text="Exit",command=lambda: exit())
+btn.pack(side=tk.LEFT, padx=10)
+
+root.title("image Browser")
+root.geometry("300x500")
+root.mainloop()
 
 img=cv2.imread("image.jpg")
 window_name='imagine RGB'
